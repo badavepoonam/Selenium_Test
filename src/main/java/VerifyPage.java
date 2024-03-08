1cac830b63926f62a9436bb81b1627ffbb7f3ef4
poonam <badavepoonam5@gmail.com>
2024-03-08 15:10:16 +0530

    Test

--- src/main/java/VerifyPage.java
+++ src/main/java/VerifyPage.java
@@ -1,19 +1,52 @@
-package org.example;
-
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 
-public class VerifyPage extends Basic {
+public class VerifyPage  {
+
+    public WebDriver driver;
+    @BeforeTest
+    public void LaunchBrowser()
+    {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Poonam\\IdeaProjects\\Coding_Test\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://www.entrata.com/");
+        driver.manage().window().maximize();
+    }
 
     @Test
     public void verifyPageTitle()
     {
-       Basic.LaunchBrowser();
-       driver.findElement(By.id("rcc-confirm-button")).click();
-        String expectedTitle = "Propery Management Software";
+
+        driver.findElement(By.className("cookie-accept-button")).click();
+        String expectedTitle = "Property Management Software | Entrata";
         String actualTitle = driver.getTitle();
         Assert.assertEquals(actualTitle, expectedTitle);
     }
+
+    @Test
+    public void VerifySignIn()
+    {
+        driver.findElement(By.className("outline-dark-button")).click();
+//        driver.findElement(By.className("cta-link-default")).click();
+        String expectedTitle = "Entrata Sign In";
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(actualTitle, expectedTitle);
+    }
+
+    @Test
+    public void ResidentLogin()
+    {
+        driver.findElement(By.xpath("//a[text()='Resident Login']")).click();
+        String expectedTitle = "Welcome to Resident Portal";
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(actualTitle, expectedTitle);
+
+    }
+
 }
--- src/main/java/VerifyWatchDemo.java
+++ src/main/java/VerifyWatchDemo.java
@@ -1,2 +1,58 @@
-package PACKAGE_NAME;public class VerifyWatchDemo {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class VerifyWatchDemo {
+
+    public WebDriver driver;
+    @BeforeTest
+    public void LaunchBrowser()
+    {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Poonam\\IdeaProjects\\Coding_Test\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://www.entrata.com/");
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void verifyPageTitle()
+    {
+
+        driver.findElement(By.className("cookie-accept-button")).click();
+        String expectedTitle = "Property Management Software | Entrata";
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(actualTitle, expectedTitle);
+    }
+
+    @Test
+    public void clickOnwatchDemo()
+    {
+        driver.findElement(By.className("header-desktop-buttons")).click();
+        String expectedTitle = "Property Management Software | Entrata";
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(actualTitle, expectedTitle);
+    }
+
+    @Test
+    public void AddDetails()
+    {
+        driver.findElement(By.id("FirstName")).sendKeys("Test");
+        driver.findElement(By.id("LastName")).sendKeys("User");
+        driver.findElement(By.id("Email")).sendKeys("abc@entrata.com");
+        driver.findElement(By.id("Company")).sendKeys("entrata");
+        driver.findElement(By.id("Phone")).sendKeys("1234567891");
+        Select drpCountry = new Select(driver.findElement(By.id("Unit_Count__c")));
+        drpCountry.selectByVisibleText("1-10");
+        driver.findElement(By.id("Title")).sendKeys("Software Engineer");
+        Select name = new Select(driver.findElement(By.id("demoRequest")));
+        name.selectByVisibleText("Resident");
+        driver.findElement(By.className("mktoButton")).click();
+
+    }
+
 }
